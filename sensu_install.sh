cadf7df6731ec1b2c45ef92299190abc158a72bc
root <root@ubuntu-14.04-amd64-vbox>
2015-05-17 16:46:28 +0000

    inital commit

---
+++ sensu_install.sh
@@ -0,0 +1,34 @@
+apt-get update
+adduser --disabled-password --gecos "" sensu
+echo "sensu ALL=(ALL:ALL) ALL" | (EDITOR="tee -a" visudo)
+echo "deb http://www.rabbitmq.com/debian/ testing main" | tee -a /etc/apt/sources.list.d/rabbitmq.list
+curl -L -o ~/rabbitmq-signing-key-public.asc http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
+apt-key add ~/rabbitmq-signing-key-public.asc
+apt-get install -y rabbitmq-server erlang-nox
+service rabbitmq-server start
+
+# ssl setup
+mkdir -p /etc/rabbitmq/ssl
+cp ./ssl_certs/sensu_ca/cacert.pem ./ssl_certs/server/cert.pem ./ssl_certs/server/key.pem /etc/rabbitmq/ssl
+cp ./config/sensu_server/rabbitmq.config  /etc/rabbitmq/.
+service rabbitmq-server restart
+rabbitmqctl add_vhost /sensu
+rabbitmqctl add_user sensu pass
+rabbitmqctl set_permissions -p /sensu sensu ".*" ".*" ".*"
+apt-get -y install redis-server
+service redis-server start
+wget -q http://repos.sensuapp.org/apt/pubkey.gpg -O- | sudo apt-key add -
+echo "deb     http://repos.sensuapp.org/apt sensu main" | tee -a /etc/apt/sources.list.d/sensu.list
+apt-get install -y sensu uchiwa
+mkdir -p /etc/sensu/ssl
+cp ./ssl_certs/client/cert.pem ./ssl_certs/client/key.pem /etc/sensu/ssl
+mkdir -p /etc/sensu/conf.d
+cp ./config/sensu_server/rabbitmq.json /etc/sensu/conf.d
+cp ./config/sensu_server/redis.json /etc/sensu/conf.d
+cp ./config/sensu_server/api.json /etc/sensu/conf.d
+cp ./config/sensu_server/uchiwa.json /etc/sensu/conf.d
+cp ./config/sensu_server/client.json /etc/sensu/conf.d
+service sensu-server start
+service sensu-client start
+service sensu-api start
+service uchiwa start
